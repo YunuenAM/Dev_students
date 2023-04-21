2202ba55a74ce708cc2241d47b4158c899bda650
Yunuen AM <yunameza@outlook.com>
2023-04-21 17:13:51 -0600

    Feat: New Version pf app.js, hard code DB, new styles

--- app.js
+++ app.js
@@ -1,232 +1,137 @@
-class Student{
-    constructor( Name,lastName,age,subject,group){
-     
-      this.Name = Name
-      this.lastName = lastName
-      this.age = age
-      this.subject = subject
-      this.group = group
-     
+//Selectors
+
+const Name = document.querySelector ('#Name');
+const lastName = document.querySelector('#lastName');
+const age = document.querySelector('#age');
+const subject = document.querySelector('#subject');
+const group = document.querySelector('#group');
+
+//Data for the searching
+
+const searchingData = { //Valores true & false
+
+    Name: '',
+    lastName: '',
+    age: '',
+    subject: '',
+    group: ''
+
+}
+
+document.addEventListener('DOMContentLoaded',()=>{
+    showStudents(students);
+});
+
+//EventListener to form
+
+Name.addEventListener('input', e=>{
+    searchingData.Name = e.target.value;
+
+    //To call the function to filter the data
+    filterStudent();
+});
+
+lastName.addEventListener('input', e =>{
+    searchingData.lastName = e.target.value;
+
+    filterStudent();
+});
+
+age.addEventListener('input', e=>{
+    searchingData.age = e.target.value;
+    filterStudent();
+});
+
+subject.addEventListener('input', e=>{
+    searchingData.subject = e.target.value;
+    filterStudent();
+});
+
+group.addEventListener('input', e=>{
+    searchingData.group = e.target.value;
+    filterStudent();
+
+});
+
+//Functions
+
+function clearHTML(){
+    //to read the result
+    const container = document.querySelector('#result');
+
+    //to clear the previous results
+
+    while(container.firstChild){
+        container.removeChild(container.firstChild);
     }
+}
 
-    calculateAverage() {
+function showStudents(students){
+    clearHTML();
 
-      this.average = (this.note1 + this.note2 + this.note3) /3;
+    //To read the result
+    const container = document.querySelector('#result');
 
+    //To construct the students HTML
 
-    if(this.average >8){
-     return("<span class='green'>Approved</span>" )
+    students.forEach(student =>{
+        const studentHTML = document.createElement('p');
+        studentHTML.innerHTML = `
+        <p>${student.Name} ${student.lastName} ${student.age} ${student.subject} ${student.group}</p>`;
+
+        container.appendChild(studentHTML);
+    })
+}
+
+function noResult(){
+    clearHTML();
+
+    const noResult = document.createElement('div');
+    noResult.classList.add('alert', 'error');
+    noResult.appendChild(document.createTextNode('No results found'));
+    document.querySelector('#result').appendChild(noResult);
+}
+
+function filterStudent(){
+    const result = students.filter(filterName).filter(filterLastName).filter(filterAge).filter(filterSubject).filter(filterGroup);
+
+    if (result.length){
+        showStudents(result);
+    
     }else{
-      return ("<span class='red'>Not Approved</span>")
+        noResult();
     }
-    return(this.average >8);
-
-  }
-
-
-  componente(){
-    return `
-    <div style='background-color:${(this.average>7)?" #FFE521": "#FFE521"};border: 1px solid ${(this.average>7)?"#FFE521": "#FFE521"}'>
-        <h4>${this.Name} </br> ${this.lastName} </br>  ${this.age}  </br>  ${this.subject} </br>  ${this.group} </h4>
-        
-     
-        
-      
-    </div>
-    `
-  }
 }
 
+//To apply filters
 
-
-
-
-const list = [
-    new Student("Yunuen", "Acosta",20,"Spanish 2", "Group 1-A"),
-    new Student ("Carlos", "Peralta", 30, "Programming 3", "Group 2-B"),
-    new Student ("Fátima", "Fernández",40, "Algebra",  "Group 3-C")
-]
-
-
-
-
-
-        let DB;
-    
-        const formulario = document.querySelector('#formulario');
-    
-        document.addEventListener('DOMContentLoaded', () => {
-            formulario.addEventListener('submit', validarEstudiante);
-    
-            conectarDB();
-        });
-    
-        function conectarDB() {
-            // ABRIR CONEXIÓN EN LA BD:
-    
-            let abrirConexion = window.indexedDB.open('crm', 1);
-    
-            // si hay un error, lanzarlo
-            abrirConexion.onerror = function() {
-                console.log('Hubo un error');
-            };
-        
-            // si todo esta bien, asignar a database el resultado
-            abrirConexion.onsuccess = function() {
-                // guardamos el resultado
-                DB = abrirConexion.result;
-            };
-        }
-    
-    function validarEstudiante(e){
-        e.preventDefault();
-    
-    
-        const Name = document.querySelector("#t2").value;
-        const lastName = document.querySelector("#t3").value;
-        const age = document. querySelector("#t4").value;
-        const subject = document.querySelector("#t5").value;
-        const group = document.querySelector("#t6").value;
-    
-         if (Name ===" " || lastName === "" || age === ""||subject===""|| group==="" ){
-            return;
-         }
-    
-    
-        const Student = {
-    
-            Name,
-            lastName,
-            age,
-            subject,
-            group
-    
-        };
-    
-        //Generar un ID único
-    
-        Student.id = Date.now();
-    
-           crearNuevoEstudiante(Student);
-    }
-    
-    function crearNuevoEstudiante(Student){
-        //nuevo
-    
-        const transaction = DB.transaction(["crm"], "readwrite");
-        const objectStore = transaction.objectStore("crm");
-        
-        objectStore.add(Student);
-    
-        transaction.oncomplete = () =>{
-            console.log("estudiante agregado");
-    
-            //Mostrar mensaje que todo está bien
-            imprimirAlerta ("Se agregó correctamente");
-    
-            setTimeout(() =>{
-                window.location.href ="index.html";
-            }, 3000);
-        };
-    
-        transaction.onerror = ()=> {
-            console.log("Hubo un error!");
-            imprimirAlerta("Hubo un error");
-        };
-    }
-    
-    function imprimirAlerta(mensaje,tipo){
-    
-        //Crear div
-    
-        const divMensaje = document.createElement('div');
-        divMensaje.classList.add( "px-4", "py-3", "rounded",  "max-w-lg", "mx-auto", "mt-6", "text-center" );
-    
-        if(tipo === 'error') {
-           divMensaje.classList.add('bg-red-100', "border-red-400", "text-red-700");
-        } else {
-            divMensaje.classList.add('bg-green-100', "border-green-400", "text-green-700");
-        }
-        
-        // Mensaje de error
-        divMensaje.textContent = mensaje;
-    
-        // Insertar en el DOM
-       formulario.appendChild(divMensaje);
-    
-        // Quitar el alert despues de 3 segundos
-        setTimeout( () => {
-            divMensaje.remove();
-        }, 3000);
-    }
-    
-  
-    
-    
-
-//////////////////////////////////////////
-
-// //Send button
-//Por cada botón agregar addEventListener
-const sendButton = document.getElementById("#send-note");
-let button = document.querySelectorAll(".button-check")
-button.forEach((button)=>{
-  button.addEventListener ("click",()=>{
-    let students = " "
-    let componente = ""
-    // alert("Hola");
-
-    try{
-      prevRes =  parseInt(document.querySelector(`#` + button.dataset.id).value);
-       {
-         
-        }
-
-    
-            students =  ( prevRes  >=8 ) ? "<span class='green'>Approved</span>" :"<span class='red'> Not Approved</span>"
-           }catch(e) {
-              result = "Enter a valid number";
-             info = "Please enter a valid number"
-             console.log(e)
-         }
-
-        let Name = document.querySelector("#t2").value;
-        let lastName = document.querySelector("#t3").value;
-        let age = document.querySelector("#t4").value;
-        let subject = document.querySelector("#t5").value;
-        let group = document.querySelector("#t6").value;
-
-
-         openModal (students,componente);
-         let divisionStudents =document.querySelector("#globalList");
-
-         let student = new Student (Name,lastName,age, subject, group);
-         student.calculateAverage();
-         list.push(student)
-         divisionStudents.innerHTML+=student.componente();
-         localStorage.setItem("listStudents",JSON.stringify(list));
-    
-
-        })
-         
-})
-
-
-
-
-const openModal = (ap,i)=>{
-document.querySelector(".global-list").innerHTML = ap
-
-
-
-
+function filterName(student){
+    if(searchingData.Name){
+        return student.Name ==searchingData.Name;
+    }return student;
 }
 
-        
+function filterLastName(student){
+    if(searchingData.lastName){
+        return student.lastName ==searchingData.lastName;
+    } return(student);
+}
 
+function filterAge(student){
+    if(searchingData.age){
+        return student.age == searchingData.age;
+    } return(student);
+}
 
+function filterSubject(student){
+    if(searchingData.subject){
+        return student.subject ==searchingData.subject;
 
+    } return (student);
+}
 
-
-
-
+function filterGroup(student){
+    if (searchingData.group){
+        return student.group == searchingData.group;
+    }return(student);
+}
--- db.js
+++ db.js
@@ -1,29 +1,113 @@
 const students =[
-{
-    Name: 'Juan',
-    lastName: 'Valderas' ,
-    age: 23 ,
-    subject: 'Español',
-    group: '1-A'
+    {
+        Name: 'Yunuen',
+        lastName: 'Acosta',
+        age: '32 years old',
+        subject: 'English',
+        group: '1-A'
 
-},
+    },
 
-{
-    Name: 'Juana',
-    lastName: 'Valencia' ,
-    age: 30 ,
-    subject: 'Programación',
-    group: '2-A'
+   
+    {
+        Name: 'Yunuen',
+        lastName: 'Gutiérrez',
+        age: '32 years old',
+        subject: 'Programming',
+        group: '1-B'
 
-},
+    },
 
-{
-    Name: 'Yunuen',
-    lastName: 'Acosta' ,
-    age: 31 ,
-    subject: 'Ciencias de la Computación',
-    group: '3-B'
+    {
+        Name: 'David',
+        lastName: 'Juárez',
+        age: '20 years old',
+        subject: 'Programming',
+        group: '1-B'
 
-},
+    },
 
-]
+    {
+        Name: 'David',
+        lastName: 'Vélez',
+        age: '20 years old',
+        subject: 'English',
+        group: '1-A'
+
+    },
+
+    {
+        Name: 'David',
+        lastName: 'Maya',
+        age: '32 years old',
+        subject: 'JavaScript',
+        group: '2-A'
+
+    },
+
+    {
+        Name: 'Marco',
+        lastName: 'Peralta',
+        age: '30 years old',
+        subject: 'Math',
+        group: '2-B'
+
+    },
+
+    {
+        Name: 'Marco',
+        lastName: 'Peralta',
+        age: '32 years old',
+        subject: 'English',
+        group: '1-A'
+
+    },
+
+    {
+        Name: 'Génesis',
+        lastName: 'Maya',
+        age: '21 years old',
+        subject: 'JavaScript',
+        group: '2-A'
+
+    },
+    {
+        Name: 'Génesis',
+        lastName: 'Maya',
+        age: '21 years old',
+        subject: 'Programming',
+        group: '1-B'
+
+    },
+
+    {
+        Name: 'Génesis',
+        lastName: 'Maya',
+        age: '21 years old',
+        subject: 'English',
+        group: '1-A'
+
+    },
+
+    {
+        Name: 'Zaim',
+        lastName: 'Vélez',
+        age: '27 years old',
+        subject: 'English',
+        group: '1-A'
+
+    },
+
+    {
+        Name: 'Zaim',
+        lastName: 'Acosta',
+        age: '25 years old',
+        subject: 'Math',
+        group: '2-B'
+
+    },
+
+
+
+
+];
